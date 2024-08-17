93997e9dfac4afce6f6c6394546f9fb382ee61b3
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-17 19:07:54 -0300

    feat: bcrypt, knex migrations, user update controller, update route user

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,18 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
-    useNullAsDefault: true
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
   },
 };
