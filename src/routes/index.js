96ea68cc303822217a73a4859d9bf77eac43784f
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-19 18:42:51 -0300

    feat: session routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
 
 const usersRouter = require("./users.routes.js")
+const sessionsRouter = require("./sessions.routes.js")
 
 const routes = Router()
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes
