b250e60d1aaedf4d9772c30a7079ace710c9ad8d
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-19 18:53:34 -0300

    feat: jwt authconfig

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,12 +1,14 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth")
+const {sign} = require("jsonwebtoken")
 
 class SessionsController {
   async create(req, res) {
     const { email, password } = req.body;
 
-    const [user] = await knex("users").where({ email }).first();
+    const user = await knex("users").where({ email }).first();
 
     if (!user) {
       throw new AppError("E-mail e/ou senha inválidos!", 401);
@@ -17,7 +19,14 @@ class SessionsController {
       throw new AppError("E-mail e/ou senha inválidos!", 401)
     }
 
-    return res.json(user);
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role }, secret, {
+      subject: String(user.id),
+      expiresIn
+    });
+
+    res.status(201).json({ token, user });
   }
 }
 
